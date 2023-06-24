025d21f579a31f22f7f8aa217f5e56bf764f7a90
Kluvens <95273765+Kluvens@users.noreply.github.com>
2023-06-24 11:05:03 +1000

    added Fluent API

--- EventManagementAPI/Context/MySqlContext.cs
+++ EventManagementAPI/Context/MySqlContext.cs
@@ -8,6 +8,20 @@ namespace EventManagementAPI.Context
         public DbSet<Event> Events { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>()
+                .HasOne(a => a.User)
+                .WithOne(u => u.Address)
+                .HasForeignKey<User>(u => u.AddressId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Address>()
+                .HasOne(a => a.Event)
+                .WithOne(e => e.Address)
+                .HasForeignKey<Event>(e => e.AddressId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
     }
 }
