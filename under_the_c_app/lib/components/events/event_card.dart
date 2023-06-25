3f8120acaf97a2b186ac2048ac101f3d4f6e7c10
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-06-25 14:26:34 +1000

    fixed the image problem

--- under_the_c_app/lib/components/events/event_card.dart
+++ under_the_c_app/lib/components/events/event_card.dart
@@ -6,73 +6,71 @@ class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 135,
+        height: 200,
+        width: 350,
         child: Card(
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
           color: const Color.fromARGB(255, 189, 192, 245),
-          child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
             Flexible(
               flex: 1,
               child: ClipRRect(
                 // borderRadius: BorderRadius.circular(15.0),
                 borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(15),
-                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
                 ),
                 child: Image.asset(
                   'images/events/money-event.jpg',
                   // width: 125,
-                  // height: 130,
+                  // height: 200,
                   fit: BoxFit.cover,
                 ),
               ),
-              // child: AspectRatio(
-              //   aspectRatio: 1 / 1,
-              //   child: ClipRRect(
-              //     // borderRadius: BorderRadius.circular(15.0),
-              //     borderRadius: const BorderRadius.only(
-              //       topLeft: Radius.circular(15),
-              //       bottomLeft: Radius.circular(15),
-              //     ),
-              //     child: Image.asset(
-              //       'images/events/money-event.jpg',
-              //       // width: 125,
-              //       // height: 130,
-              //       fit: BoxFit.cover,
-              //     ),
-              //   ),
-              // ),
             ),
             Flexible(
-                flex: 2,
-                child: ListTile(
-                  title: const Text(
-                    'Entertainment Event hehe',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: const Padding(
-                      padding: EdgeInsets.only(top: 15),
-                      child: Column(children: [
-                        Text('Alexdander venue, NSW'),
-                        Padding(
-                          padding: EdgeInsets.only(top: 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('1 June 2023'),
-                              Text('3 dollars'),
-                            ],
-                          ),
-                        )
-                      ])),
-                  trailing: IconButton(
-                      icon: const Icon(Icons.favorite_border),
-                      onPressed: () {
-                        // ignore: avoid_print
-                        print('fav button clicked');
-                      }),
+                flex: 1,
+                child: Row(
+                  children: [
+                    // const Text("Hello"),
+                    Expanded(
+                        child: ListTile(
+                      title: const Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Text(
+                            'Entertainment Event hehe',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold),
+                          )),
+                      subtitle: const Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Column(children: [
+                            Padding(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Thus 10:15 - New York, NY 2023',
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                  // Text('3'),
+                                ],
+                              ),
+                            )
+                          ])),
+                      trailing: IconButton(
+                          icon: const Icon(Icons.favorite_border),
+                          onPressed: () {
+                            // ignore: avoid_print
+                            print('fav button clicked');
+                          }),
+                    ))
+                  ],
                 ))
           ]),
         ));
