bedc7cc42b799a5bcdb0b6a303cfcca458755d4e
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-06-25 16:13:11 +1000

    refactored the event card so there are multiple widgets

--- under_the_c_app/lib/components/events/event_card.dart
+++ under_the_c_app/lib/components/events/event_card.dart
@@ -1,5 +1,129 @@
 import 'package:flutter/material.dart';
 
+class EventImage extends StatelessWidget {
+  const EventImage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: ClipRRect(
+        // borderRadius: BorderRadius.circular(15.0),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+        child: Image.asset(
+          'images/events/money-event.jpg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class EventDate extends StatelessWidget {
+  const EventDate({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 161, 163, 210),
+            borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: Column(children: [
+              Text(
+                "APR",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "20",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              )
+            ]),
+          ),
+        ));
+  }
+}
+
+class EventDescription extends StatelessWidget {
+  const EventDescription({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 3,
+      child: ListTile(
+        title: const Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Text(
+            'Entertainment Event',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+          ),
+        ),
+        subtitle: const Padding(
+          padding: EdgeInsets.only(top: 1, left: 12),
+          child: EventSubtitle(),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.favorite_border),
+          onPressed: () {
+            print('fav button clicked');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class EventSubtitle extends StatelessWidget {
+  const EventSubtitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+        padding: EdgeInsets.only(top: 1, left: 12),
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Thus 10:15 - New York, NY',
+                  style: TextStyle(fontSize: 10),
+                ),
+                // Text('3'),
+              ],
+            ),
+          )
+        ]));
+  }
+}
+
+class EventDetails extends StatelessWidget {
+  const EventDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Flexible(
+        flex: 1,
+        child: Row(
+          children: [EventDate(), EventSubtitle()],
+        ));
+  }
+}
+
 class EventCard extends StatelessWidget {
   const EventCard({Key? key}) : super(key: key);
 
@@ -12,98 +136,19 @@ class EventCard extends StatelessWidget {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
           color: const Color.fromARGB(255, 189, 192, 245),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Flexible(
-              flex: 1,
-              child: ClipRRect(
-                // borderRadius: BorderRadius.circular(15.0),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-                child: Image.asset(
-                  'images/events/money-event.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Flexible(
-                flex: 1,
-                child: Row(
+          child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                EventImage(),
+                Expanded(
+                    child: Row(
                   children: [
-                    Flexible(
-                        flex: 1,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 15),
-                          child: Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                  color:
-                                      const Color.fromARGB(255, 161, 163, 210),
-                                  borderRadius: BorderRadius.circular(8)),
-                              child: const Center(
-                                child: Padding(
-                                  padding: EdgeInsets.only(top: 5),
-                                  child: Column(children: [
-                                    Text(
-                                      "APR",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                    Text(
-                                      "20",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 24,
-                                      ),
-                                    )
-                                  ]),
-                                ),
-                              )),
-                        )),
-                    Flexible(
-                        flex: 3,
-                        child: ListTile(
-                          title: const Padding(
-                              padding: EdgeInsets.only(top: 10),
-                              child: Text(
-                                'Entertainment Event',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 19, fontWeight: FontWeight.bold),
-                              )),
-                          subtitle: const Padding(
-                              padding: EdgeInsets.only(top: 1, left: 12),
-                              child: Column(children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 10),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Thus 10:15 - New York, NY 2023',
-                                        style: TextStyle(fontSize: 10),
-                                      ),
-                                      // Text('3'),
-                                    ],
-                                  ),
-                                )
-                              ])),
-                          trailing: IconButton(
-                              icon: const Icon(Icons.favorite_border),
-                              onPressed: () {
-                                // ignore: avoid_print
-                                print('fav button clicked');
-                              }),
-                        ))
+                    Padding(
+                        padding: EdgeInsets.only(left: 13), child: EventDate()),
+                    EventDescription()
                   ],
                 ))
-          ]),
+              ]),
         ));
   }
 }
