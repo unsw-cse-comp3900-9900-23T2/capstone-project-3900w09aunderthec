a7c78e713b54e6accddffecc64f58f52d967ebf1
ASEANITdirector <59725343+HardcoreTrash@users.noreply.github.com>
2023-06-26 03:08:14 +1000

    Current draft of event creation

---
+++ under_the_c_app/lib/components/hover.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+// https://stackoverflow.com/questions/73770245/how-to-hover-text-in-flutter
+
+class HoverBuilder extends StatefulWidget {
+  const HoverBuilder({
+    required this.builder,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget Function(bool isHovered) builder;
+
+  @override
+  State<HoverBuilder> createState() => _HoverBuilderState();
+}
+
+class _HoverBuilderState extends State<HoverBuilder> {
+  bool _isHovered = false;
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (PointerEnterEvent event) => _onHoverChanged(enabled: true),
+      onExit: (PointerExitEvent event) => _onHoverChanged(enabled: false),
+      child: widget.builder(_isHovered),
+    );
+  }
+
+  void _onHoverChanged({required bool enabled}) {
+    setState(() {
+      _isHovered = enabled;
+    });
+  }
+}
--- under_the_c_app/lib/pages/event.dart
+++ under_the_c_app/lib/pages/event.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/toggle_button.dart';
+// import 'toggle_button.dart';
+// import 'hover.dart';
+// import 'create_event.dart';
 
 class EventPage extends StatelessWidget {
   const EventPage({Key? key}) : super(key: key);
@@ -11,3 +15,42 @@ class EventPage extends StatelessWidget {
         child: const Text("Event Page"));
   }
 }
+// void main() {
+//   // runApp(const MyApp());
+//   runApp(const MaterialApp(
+//     title: "Create Event",
+//     home: HomeRoute(),
+//   ));
+// }
+
+// class HomeRoute extends StatelessWidget {
+//   const HomeRoute({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Home"),
+//         actions: [
+//           Padding(
+//             padding: const EdgeInsets.only(right: 20.0),
+//             child: GestureDetector(onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                     builder: (context) => const CreateEventRoute()),
+//               );
+//             }, child: HoverBuilder(builder: (isHovered) {
+//               return Icon(
+//                 Icons.add_outlined,
+//                 size: 20.0,
+//                 color: isHovered ? Colors.grey : Colors.white,
+//                 // child: const Text("Create event"),
+//               );
+//             })),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
