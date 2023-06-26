27f1012d2e44026d593916e0d0aeae0791153a32
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-06-26 17:35:36 +1000

    Restructured the event_details class so that it has a divisor and have paddings

--- under_the_c_app/lib/pages/subpages/event_details.dart
+++ under_the_c_app/lib/pages/subpages/event_details.dart
@@ -21,37 +21,60 @@ class EventDetailsPage extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Column(children: [
-        Image.asset(
-          event.imageUrl,
-          fit: BoxFit.cover,
-          height: 250,
-          width: MediaQuery.of(context).size.width,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 20, left: 30),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  event.title,
-                  style: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 0.6),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  '${getFirstThreeLettersWeekday(event.time)} ${getMonthName(event.time)} ${getDay(event.time)} ${getYear(event.time)} at ${getTime(event.time)}',
-                  style: const TextStyle(fontSize: 12, letterSpacing: 0.2),
-                )
-              ],
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            event.imageUrl,
+            fit: BoxFit.cover,
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30, bottom: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    event.title,
+                    style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.6),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '${getFirstThreeLettersWeekday(event.time)} ${getMonthName(event.time)} ${getDay(event.time)} ${getYear(event.time)} at ${getTime(event.time)}',
+                    style: const TextStyle(fontSize: 12, letterSpacing: 0.2),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      ]),
+          const Divider(
+            color: Color.fromARGB(221, 147, 147, 147),
+            height: 20,
+            thickness: 1,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 4, left: 30, bottom: 4),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "EVENT DETAILS",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromARGB(221, 5, 5, 5),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
