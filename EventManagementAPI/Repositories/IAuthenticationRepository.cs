b553f9561127c3b61dfb6f8a775c43fe339b2bee
stevenoid01 <stevedong64@gmail.com>
2023-06-28 01:25:12 +1000

    revert interface

--- EventManagementAPI/Repositories/IAuthenticationRepository.cs
+++ EventManagementAPI/Repositories/IAuthenticationRepository.cs
@@ -11,7 +11,6 @@ namespace EventManagementAPI.Repositories
         Task<bool> checkDuplicateEmails(String email);
         Task createUser(String username, String email, Boolean isHost);
         Task<bool> getUserType(String email);
-        Task<int> getUid(String email);
     }
 }
 
