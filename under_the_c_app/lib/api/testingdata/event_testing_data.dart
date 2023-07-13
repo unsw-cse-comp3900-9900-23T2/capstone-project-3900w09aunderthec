2a9bc02114822957f7d3fec2fef9325d4a4f5f97
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-07-13 23:41:16 +1000

    Deleted unused stuffs in the event testing data

--- under_the_c_app/lib/api/testingdata/event_testing_data.dart
+++ under_the_c_app/lib/api/testingdata/event_testing_data.dart
@@ -125,41 +125,9 @@ Future<List<Event>> fetchAllHostedEvents() async {
   return hostedEvents;
 }
 
-// TODO: event.dart: It's fetching fake data, need to replace with real data
 Future<Event> fetchHostedEventById(String eventId) async {
   final event = incomingEvents.firstWhere((e) => e.eventId == eventId,
       orElse: () => throw Exception('Event not found'));
   return event;
 }
 
-// void getEvents() async {
-//   HttpClient client = HttpClient();
-//   client.badCertificateCallback =
-//       ((X509Certificate cert, String host, int port) => true);
-//   var ioClient = IOClient(client);
-
-//   final registerUrl = Uri.https('10.0.2.2:7161', '/EventDisplay/ListEvents');
-
-//   try {
-//     final response = await ioClient.post(
-//       registerUrl,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         'Content-Type': 'application/json',
-//         'Accept': '*/*'
-//       },
-//       body: jsonEncode({
-//         // TODO: [PLHV-157] event.dart:getEvents(): change UID to possibly the email
-//         "uid": "1"
-//       }),
-//     );
-
-//     // server currently returns a 500 as its not implemented
-//     if (response.statusCode == 500) {
-//       print(response.body);
-//       throw Exception(response.body);
-//     }
-//   } catch (e) {
-//     print('An error occured: $e');
-//   }
-// }
