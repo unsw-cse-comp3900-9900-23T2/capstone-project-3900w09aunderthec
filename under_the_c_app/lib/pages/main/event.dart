ba7cb091439c0e1765a62ece863f29cf286eee60
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-07-15 08:58:47 +1000

    connected the add event button to the route

--- under_the_c_app/lib/pages/main/event.dart
+++ under_the_c_app/lib/pages/main/event.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:under_the_c_app/api/testingdata/event_testing_data.dart';
 import 'package:under_the_c_app/components/events/event_card.dart';
+import 'package:under_the_c_app/config/routes.dart';
 
 class EventPage extends StatelessWidget {
   final bool isHost;
@@ -61,7 +62,6 @@ class EventPage extends StatelessWidget {
           ),
         ),
         Positioned(
-          // left: 0,
           right: 0,
           bottom: 0,
           child: Padding(
@@ -72,7 +72,7 @@ class EventPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20)),
               child: IconButton(
                   onPressed: () {
-                    context.go('/event_booking/1');
+                    context.go(AppRoutes.eventAdd);
                   },
                   icon: const Icon(Icons.add),
                   iconSize: 50,
