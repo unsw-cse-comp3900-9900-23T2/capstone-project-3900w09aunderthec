c32ea7691fd858a193d051fafdbfff7a3aad07d1
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-07-15 22:50:29 +1000

    Added event converter

--- under_the_c_app/lib/api/converters/event_converter.dart
+++ under_the_c_app/lib/api/converters/event_converter.dart
@@ -4,17 +4,19 @@ import 'package:under_the_c_app/types/address.dart';
 List<Event> BackendDataEventListToEvent(data) {
   List<Event> events = [];
   for (var event in data) {
-    events.add(Event(
-      hostuid: event.hosterFK.toString(),
-      title: event.title,
-      eventId: event.eventId.toString(),
-      imageUrl: 'images/events/money-event.jpg',
-      time: event.time.toString(),
-      venue: event.venue,
-      // TODO: [PLHV-198] EventCreate: We need set up price in the backend, can't always have price = 0.
-      price: 0,
-      description: event.description,
-    ));
+    events.add(
+      Event(
+        hostuid: event.hosterFK.toString(),
+        title: event.title,
+        eventId: event.eventId.toString(),
+        imageUrl: 'images/events/money-event.jpg',
+        time: event.time.toString(),
+        venue: event.venue,
+        // TODO: [PLHV-198] EventCreate: We need set up price in the backend, can't always have price = 0.
+        price: 0,
+        description: event.description,
+      ),
+    );
   }
   return events;
 }
