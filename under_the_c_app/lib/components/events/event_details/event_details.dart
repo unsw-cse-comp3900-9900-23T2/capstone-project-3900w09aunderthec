aad05d55edcad8643768996b6fed44d821831e7d
stevenoid01 <stevedong64@gmail.com>
2023-07-18 20:57:10 +1000

    added create tickets button

--- under_the_c_app/lib/components/events/event_details/event_details.dart
+++ under_the_c_app/lib/components/events/event_details/event_details.dart
@@ -5,6 +5,7 @@ import 'package:under_the_c_app/components/events/event_details/comment/comment.
 import 'package:under_the_c_app/components/events/event_details/price.dart';
 import 'package:under_the_c_app/components/functions/time/time_converter.dart';
 import 'package:under_the_c_app/config/routes/routes.dart';
+import 'package:under_the_c_app/config/session_variables.dart';
 import 'package:under_the_c_app/providers/event_providers.dart';
 
 class EventDetailsPage extends ConsumerWidget {
@@ -17,6 +18,7 @@ class EventDetailsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final event = ref.watch(eventProvider(eventId));
+
     return event.when(
         data: (event) {
           return Scaffold(
@@ -132,17 +134,26 @@ class EventDetailsPage extends ConsumerWidget {
                     padding: const EdgeInsets.all(30),
                     child: ElevatedButton(
                       onPressed: () {
-                        context.go(AppRoutes.eventBook(
-                            event.eventId!, event.title, event.venue));
+                        if (sessionVariables.sessionIsHost &&
+                            sessionVariables.navLocation == 0) {
+                          context.go(AppRoutes.eventBook(
+                              event.eventId!, event.title, event.venue));
+                        } else {
+                          context.go(AppRoutes.eventBook(
+                              event.eventId!, event.title, event.venue));
+                        }
                       },
                       style: TextButton.styleFrom(
-                        minimumSize: const Size(200, 0),
+                        minimumSize: const Size(150, 0),
                         padding: const EdgeInsets.all(20),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18),
                         ),
                       ),
-                      child: const Text("Buy Ticket"),
+                      child: Text(sessionVariables.sessionIsHost &&
+                              sessionVariables.navLocation == 0
+                          ? "Create Tickets"
+                          : "Buy Ticket"),
                     ),
                   ),
                 ),
--- under_the_c_app/lib/config/session_variables.dart
+++ under_the_c_app/lib/config/session_variables.dart
@@ -4,6 +4,7 @@ class sessionVariables {
   static String email = "";
   static int vipLevel = 0;
   static int loyaltyPoints = 0;
+  static int navLocation = 1;
 
   static void resetVariables() {
     sessionIsHost = true;
@@ -11,6 +12,7 @@ class sessionVariables {
     email = "";
     vipLevel = 0;
     loyaltyPoints = 0;
+    navLocation = 1;
   }
 
   sessionVariables._();
