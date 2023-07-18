360e9083872ec747dcb7a359f11eaaa4f6d6b1df
ASEANITdirector <59725343+HardcoreTrash@users.noreply.github.com>
2023-07-19 07:49:02 +1000

    Fixing session variable

--- under_the_c_app/lib/config/session_variables.dart
+++ under_the_c_app/lib/config/session_variables.dart
@@ -1,6 +1,6 @@
 class sessionVariables {
-  static bool sessionIsHost = false;
-  static int uid = 3;
+  static bool sessionIsHost = true;
+  static int uid = 1;
   static String email = "";
   static int vipLevel = 0;
   static int loyaltyPoints = 0;
