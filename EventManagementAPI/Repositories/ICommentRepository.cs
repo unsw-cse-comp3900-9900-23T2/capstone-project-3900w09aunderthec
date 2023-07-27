323098033f310329c21b220391b963cea1bb5544
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-07-27 18:08:57 +1000

    Added isLikeComment, isDislikeCommentAPI, tested

--- EventManagementAPI/Repositories/ICommentRepository.cs
+++ EventManagementAPI/Repositories/ICommentRepository.cs
@@ -11,6 +11,8 @@ namespace EventManagementAPI.Repositories
         public Task<CommentListingDto> GetComments(string? sortBy, int? eventId, int? inReplyToComment);
         public Task<Comment?> GetCommentById(int id);
         public Task<Comment?> CreateComment(int customerId, int eventId, int? commentId, string comment);
+        public Task<bool> isLikeComment(int uid, int commentId);
+        public Task<bool> isDislikeComment(int uid, int commentId);
         public Task<string> ToggleLikeComment(int customerId, int commentId);
         public Task<string> ToggleDislikeComment(int customerId, int commentId);
         public Task<Comment> RetrieveComment(int commentId);
