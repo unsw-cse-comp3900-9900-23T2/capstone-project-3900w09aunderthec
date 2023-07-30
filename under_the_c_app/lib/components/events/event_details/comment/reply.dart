e73113ac88a1e04dce62a12ffa3dd96d470f1ab2
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-07-30 10:42:06 +1000

    Added the basic layout for the reply

--- under_the_c_app/lib/components/events/event_details/comment/reply.dart
+++ under_the_c_app/lib/components/events/event_details/comment/reply.dart
@@ -5,52 +5,42 @@ class Reply extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Column(
       children: [
-        // Expanded(
-        //   child: Text("Hello aksdjf liasdjfio asdfi"),
-        // ),
-        // Container(
-        //     child: Text(
-        //         "Helelefklasdmfklasdfasdfasdfasdfaf djskfl jlksdjf lkasjdf lkasdjflk sdjfkllll"))
+        const Divider(),
+        // for profile image, name and date of publishing
+        Padding(
+          padding: const EdgeInsets.only(left: 15.0, top: 10, bottom: 5),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage("images/users/guy.jpg"),
+                radius: 16,
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text(customer.userName),
+                  const Text(
+                    "Abraham",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  SizedBox(
+                    width: screenWidth * 0.5,
+                    child: const Text(
+                      "This is the best content every of the reply laolll",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
-    // return  const Column(
-    //   children: [
-    //     Divider(),
-    //     // for profile image, name and date of publishing
-    //     Padding(
-    //       padding: EdgeInsets.only(left: 15.0, top: 10, bottom: 5),
-    //       child: Row(
-    //         children: [
-    //           CircleAvatar(
-    //             backgroundImage: AssetImage("images/users/guy.jpg"),
-    //             radius: 16,
-    //           ),
-    //           SizedBox(width: 10),
-    //           Column(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             children: [
-    //               // Text(customer.userName),
-    //               Text(
-    //                 "Abraham",
-    //                 style: TextStyle(fontSize: 12),
-    //               ),
-    //               SizedBox(height: 4),
-    //               Flexible(
-    //                 child: Text(
-    //                     "This is the best content every of the reply laolll"),
-    //               ),
-    //               // Flexible(
-    //               //   child: Text(
-    //               //       "This is the best content every of the reply laolll"),
-    //               // )
-    //             ],
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ],
-    // );
   }
 }
