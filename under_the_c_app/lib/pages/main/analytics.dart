d6f5846ca71bf506e1dd8f0b7d5a6c03afcc0279
Fengyu Wang <z5187561@ad.unsw.edu.au>
2023-08-01 12:15:40 +1000

    Added ranking summary for the analytics page

--- under_the_c_app/lib/pages/main/analytics.dart
+++ under_the_c_app/lib/pages/main/analytics.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../components/analytics/events_rank.dart';
 import '../../components/analytics/line_chart.dart';
 import '../../components/analytics/summary_cards.dart';
 
@@ -13,18 +14,39 @@ class AnalyticsPage extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(20, 30, 30, 0),
         child: Column(
           children: [
-            SummaryCards(),
-            SizedBox(height: 35),
+            const SummaryCards(),
+            const SizedBox(height: 35),
+            // for the line graph card
             Card(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(25, 25, 15, 15),
                 child: Container(
                   height: 240,
-                  // width: 200,
                   child: LineChartWidget(),
                 ),
               ),
             ),
+            const SizedBox(height: 20),
+            // for the ranking section
+            const Row(
+              children: [
+                Expanded(
+                  child: EventsRank(
+                    percentage: 0.4,
+                    color: Color.fromARGB(255, 146, 163, 255),
+                    title: 'Hosted Events',
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                    child: EventsRank(
+                  percentage: 0.2,
+                  color: Color.fromARGB(255, 250, 146, 54),
+                  title: 'Subscribers',
+                )),
+              ],
+            ),
+            SizedBox(height: 25)
           ],
         ),
       ),
